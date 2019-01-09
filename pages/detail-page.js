52a03388475be004d739ca1d4091b09763c837ab
CHARLES <charlescallaghan@gmail.com>
2019-01-09 16:15:23 +0100

    Set Up Form Validation

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,13 +1,43 @@
 // Add Event Handler
 
+const doesNotPassAllValidations = (name, msg) => {
+    if (!name) {
+        alert('Please, include your name!')
+        return true
+    }
+
+    if (!msg) {
+        alert('You have not left a message!')
+        return true
+    }
+
+    if(msg.length > 280) {
+        alert('Your comment is too long.')
+        return true
+    }
+
+    const badWords = /shit|fuck/ig;
+
+    if(msg.match(badWords)) {
+        alert('Warning: this comment has been flagged as offensive.')
+        return true
+    }
+
+    return false
+}
+
 const submitComment = () => {
 
     // Gather Data
 
     const inputField = document.getElementById('name')
     const name = inputField.value
+    const capitaliseName = name.charAt(0).toUpperCase() + name.slice(1)
     const textArea = document.getElementById('msg')
     const msg = textArea.value
+    if (doesNotPassAllValidations(name,msg)) {
+        return null
+    }
 
     // Create the Elements needed
     
@@ -17,7 +47,7 @@ const submitComment = () => {
 
     // Adjust Elements We Created
 
-    h3.innerHTML = `${name} said:`
+    h3.innerHTML = `${capitaliseName} said:`
     p.innerHTML = msg
     comment.classList.add('comment')
     comment.appendChild(h3)
